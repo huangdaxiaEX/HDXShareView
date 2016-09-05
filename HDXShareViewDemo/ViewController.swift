623ea7b1187f25b233d55d0f6825e7cb58477b23
黄凯 <527548875@qq.com>
2016-09-05 17:54:25 +0800

    初始化文件

--- HDXShareViewDemo/ViewController.swift
+++ HDXShareViewDemo/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .grayColor()
+        
+        let button = UIButton()
+        button.frame = CGRect(x: 40, y: 40, width: 100, height: 100)
+        button.backgroundColor = .orangeColor()
+        button.addTarget(self, action: #selector(didClickButton), forControlEvents: .TouchUpInside)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func didClickButton() {
+        let URL = NSURL(string: "https://developer.apple.com/")!
+        let shareModel = ShareModel(title: "share title", description: "share title", thumbnail: Images.ShareQQ, media: .URL(URL))
+        let shareView = HDXShareView(shareModel: shareModel) {
+            print("share finished")
+        }
+        shareView.show()
+    }
+    
 }
 
